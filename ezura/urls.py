244ee27e6ee0fc5da0607633a39a0cc45ea1add3
Ann <totoro.daifuku1@gmail.com>
2021-09-26 04:13:55 +0000

    ブログ機能

--- ezura/urls.py
+++ ezura/urls.py
@@ -5,4 +5,10 @@ app_name = 'ezura'
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
     path('inquiry/', views.InquiryView.as_view(), name="inquiry"),
+    path('blog-list/', views.BlogListView.as_view(), name="blog_list"),
+    path('blog-detail/<int:pk>/', views.BlogDetailView.as_view(), name="blog_detail"),
+    path('blog-create/', views.BlogCreateView.as_view(), name="blog_create"),
+    path('blog-update<int:pk>/', views.BlogUpdateView.as_view(), name="blog_update"),
+    path('blog-delete/<int:pk>/', views.BlogDeleteView.as_view(), name="blog_delete"),
+    path('all-blog-list/', views.AllBlogListView.as_view(), name="all_blog_list"),
 ]
