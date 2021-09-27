7739430b54a90047be0b1fb8578b83e46e2b220c
KazumaYamaguchi <ksw2070048@stu.o-hara.ac.jp>
2021-09-27 23:28:13 +0000

    yamaguchi

--- kazuma/views.py
+++ kazuma/views.py
@@ -1,3 +1,88 @@
 from django.shortcuts import render
+import logging
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib import messages
+from .forms import InquiryForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import kazuma
+from .forms import InquiryForm, BlogCreateForm
 
-# Create your views here.
+logger = logging.getLogger(__name__)
+
+
+class IndexView(generic.TemplateView):
+    template_name = "index.html"
+
+
+class InquiryView(generic.FormView):
+    template_name = "inquiry.html"
+    form_class = InquiryForm
+    success_url = reverse_lazy('kazuma:inquiry')
+
+    def form_valid(self, form):
+        form.send_email()
+        messages.success(self.request, 'メッセージを送信しました。')
+        logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+        return super().form_valid(form)
+
+
+class BlogListView(LoginRequiredMixin, generic.ListView):
+    model = kazuma
+    template_name = 'blog_list.html'
+    paginate_by = 2
+
+    def get_queryset(self):
+        blogs = kazuma.objects.filter(user=self.request.user).order_by('-created_at')
+        return blogs
+
+class BlogDetailView(LoginRequiredMixin, generic.DetailView):
+    model = kazuma
+    template_name = 'blog_detail.html'
+
+class BlogCreateView(LoginRequiredMixin, generic.CreateView):
+    model = kazuma
+    template_name = 'blog_create.html'
+    form_class = BlogCreateForm
+    success_url = reverse_lazy('kazuma:blog_list')
+
+    def form_valid(self, form):
+        # diary = form.save(commit=False)
+        # diary.user = self.request.user
+        # diary.save()
+        
+        kauzuma = form.save(commit=False)
+        kauzuma.user = self.request.user
+        kauzuma.save()
+
+        messages.success(self.request, '日記を作成しました。')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "日記の作成に失敗しました。")
+        return super().form_invalid(form)
+
+class BlogUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = kazuma
+    template_name = 'blog_update.html'
+    form_class = BlogCreateForm
+
+    def get_success_url(self):
+        return reverse_lazy('kazuma:blog_detail', kwargs={'pk': self.kwargs['pk']})
+
+    def form_valid(self, form):
+        messages.success(self.request, '日記を更新しました。')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "日記の更新に失敗しました。")
+        return super().form_invalid(form)
+
+class BlogDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = kazuma
+    template_name = 'blog_delete.html'
+    success_url = reverse_lazy('kazuma:blog_list')
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "日記を削除しました。")
+        return super().delete(request, *args, **kwargs)
